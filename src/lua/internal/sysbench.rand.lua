52ea234d09191df6f5e44837a98305a9bd24b698
Marco Tusa <tusamarco@gmail.com>
2023-04-19 18:24:39 +0200

    no message

--- src/lua/internal/sysbench.rand.lua
+++ src/lua/internal/sysbench.rand.lua
@@ -34,7 +34,7 @@ uint32_t sb_rand_unique(void);
 void sb_rand_str(const char *, char *);
 void sb_rand_varstr(char *, uint32_t, uint32_t);
 void sb_rand_varstr_alpha(char *, uint32_t, uint32_t);
-char* sb_rand_continent(char *, uint32_t);
+char* sb_rand_continent(uint32_t);
 double sb_rand_uniform_double(void);
 ]]
 
@@ -97,7 +97,7 @@ function sysbench.rand.varstringalpha(min_len, max_len)
 end
 
 function sysbench.rand.getcontinent(min_len)
-   assert(max_len > 0)
+   assert(min_len > 0)
    return ffi.C.sb_rand_continent(min_len)
 end
 
